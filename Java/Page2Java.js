5635df8bb7765e169e339a2d67e98e4e4085cbe2
Sunny12809 <113295292+Sunny12809@users.noreply.github.com>
2025-06-25 18:14:21 +0100

    Update Page2Java.js

--- Java/Page2Java.js
+++ Java/Page2Java.js
@@ -1,27 +1,27 @@
-const images = document.querySelectorAll('#carouselImages img');
-  let current = 0;
+const items = document.querySelectorAll('#carouselimages .carouselitem');
+let current = 0;
 
-  function updateCarousel() {
-    images.forEach((img, i) => {
-      img.className = '';
-      if (i === current) {
-        img.classList.add('active');
-      } else if (i === (current - 1 + images.length) % images.length) {
-        img.classList.add('prev');
-      } else if (i === (current + 1) % images.length) {
-        img.classList.add('next');
-      }
-    });
-  }
+function updateCarousel() {
+  items.forEach((item, i) => {
+    item.className = 'carouselitem';
+    if (i === current) {
+      item.classList.add('active');
+    } else if (i === (current - 1 + items.length) % items.length) {
+      item.classList.add('prev');
+    } else if (i === (current + 1) % items.length) {
+      item.classList.add('next');
+    }
+  });
+}
 
-  function nextSlide() {
-    current = (current + 1) % images.length;
-    updateCarousel();
-  }
-
-  function prevSlide() {
-    current = (current - 1 + images.length) % images.length;
-    updateCarousel();
-  }
-
+function nextSlide() {
+  current = (current + 1) % items.length;
   updateCarousel();
+}
+
+function prevSlide() {
+  current = (current - 1 + items.length) % items.length;
+  updateCarousel();
+}
+
+updateCarousel();
